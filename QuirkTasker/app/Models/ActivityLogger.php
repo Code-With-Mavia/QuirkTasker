3d91e3a4bd59da74288316eb625dd7d1984cb4cb
code-with-mavia <maviaqaiser09@gmail.com>
2025-11-07 12:25:24 +0500

    refractored models

--- QuirkTasker/app/Models/ActivityLogger.php
+++ QuirkTasker/app/Models/ActivityLogger.php
@@ -10,10 +10,24 @@ class ActivityLogger extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
+        'task_id',
+        'action',
+        'data',
     ];
 
     public function ActivityLogger()
     {
         return $this->belongsTo(ActivityLogger::class);
+    }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Tasks()
+    {
+        return $this->belongsTo(Tasks::class);
     }
 }
--- QuirkTasker/app/Models/Tasks.php
+++ QuirkTasker/app/Models/Tasks.php
@@ -10,10 +10,26 @@ class Tasks extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
+        'title',
+        'status',
+        'priority',
+        'due',
     ];
 
     public function Tasks()
     {
         return $this->belongsTo(Tasks::class);
     }
+
+    public function ActivityLogger()
+    {
+        return $this->belongsTo(ActivityLogger::class);
+    }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
